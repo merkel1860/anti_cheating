8f0227531efcb63b21b76970f1a23c5ea12f512d
merkel1860 <merkelmller@yandex.ru>
2021-05-05 09:06:30 -0400

    Additional functions...

--- src/core/Main.java
+++ src/core/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Cheaters!");
+        System.out.println("Welcome back!");
     }
 }
